01e694093dde25d47ec4e9b5eb5fea74ac8c1f15
Benji Shine <git@bshine.net>
2018-01-15 13:15:39 -0800

    trying to get esm to work so i can use funcadelic, but this project is gulp based, and gulp is not compatible with esm, if im reading https://github.com/gulpjs/gulp/issues/2019 right

--- js-test-harness/test/setup/node.js
+++ js-test-harness/test/setup/node.js
@@ -1,6 +1,10 @@
+require = require('@std/esm')(module)
+module.exports = require('./main.mjs').default
+
 global.chai = require('chai');
 global.sinon = require('sinon');
 global.chai.use(require('sinon-chai'));
+
 
 require('babel-core/register');
 require('./setup')();
