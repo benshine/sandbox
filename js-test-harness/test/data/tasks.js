99df697ba11f2b9f6775694b5ba23284164e856e
Benji Shine <git@bshine.net>
2018-01-30 12:25:29 -0800

    experiments in functional programming with Terence

---
+++ js-test-harness/test/data/tasks.js
@@ -0,0 +1,24 @@
+     const allTasks = {
+         date: "2017-09-22",
+         byPerson: [
+             {
+                 responsible: "EG",
+                 tasks: [
+                     {id: 111, desc: "task 111", done: false},
+                     {id: 222, desc: "task 222", done: false}
+                 ]
+             },
+             {
+                 responsible: "FK",
+                 tasks: [
+                     {id: 555, desc: "task 555", done: false},
+                     {id: 777, desc: "task 777", done: true},
+                     {id: 999, desc: "task 999", done: false}
+                 ]
+             },
+             {
+                 responsible: "ST",
+                 tasks: [{id: 444, desc: "task 444", done: true}]
+             }
+         ]
+     };
