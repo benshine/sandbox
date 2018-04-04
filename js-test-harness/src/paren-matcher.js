387ddd34b6a2d0b56fd041f4dab97f6e5fb4ae5f
Benji Shine <git@bshine.net>
2018-04-03 20:09:23 -0700

    I'm passing all my tests but I have a detected underflow

--- js-test-harness/src/paren-matcher.js
+++ js-test-harness/src/paren-matcher.js
@@ -1,10 +1,37 @@
 /**
  * based on problem at https://www.interviewcake.com/question/javascript/matching-parens?
  * @param input
- * @param index
+ * @param openParenIndex
  * @returns {null}
  */
-export default function parenMatcher(input, index) {
-  console.log({input, index});
-  return null;
+export default function parenMatcher(input, openParenIndex) {
+  const arr = input.split('');
+
+  // I'm going through serious dorking around to avoid
+  // mutating anything. I'm not sure it's worth it,
+  // except as an exercise.
+
+  let {opens, pairs} = arr.reduce((acc, curr, index) => {
+    if (curr === '(') {
+      return {opens: acc.opens.concat(index), pairs: acc.pairs };
+    }
+    if (curr === ')') {
+      // to do: detect unbalanced strings
+      const matchingOpen = acc.opens[acc.opens.length - 1];
+      return {
+        opens: acc.opens.slice(0, -1),
+        pairs: acc.pairs.concat({open: matchingOpen, close: index}) };
+    }
+    return acc;
+  }, {opens: [], pairs: []});
+
+  console.log({opens, pairs});
+
+  // Find the matching close for specified open
+  const targetPair = pairs.find(p => p.open === openParenIndex);
+
+  if (targetPair) {
+    return targetPair.close;
+  }
+  return NaN;
 }
