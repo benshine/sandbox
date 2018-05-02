4e22fd88ad90310ae15a174ecc69db11b3cb0b37
Benji Shine <git@bshine.net>
2018-05-02 15:37:24 -0700

    creadted hello-node and installed ramda

---
+++ scripts/hello-node/fun.js
@@ -0,0 +1,11 @@
+const R = require('ramda');
+
+console.log( 'hello there'  );
+
+const thing = 'nice job';
+
+for (const i of thing) {
+  console.log( `i: ${i}` );
+}
+
+console.log( {ramda: R}  );
