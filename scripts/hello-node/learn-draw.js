212a7e99c57b0a706825b8d75de7a16c3afbfd53
Benji Shine <git@bshine.net>
2018-05-02 19:00:11 -0700

    making a bit of progress leanring terminal-kit

---
+++ scripts/hello-node/learn-draw.js
@@ -0,0 +1,91 @@
+const R = require('ramda');
+//const term = require('terminal-kit').terminal;
+// const ScreenBuffer = require('terminal-kit').ScreenBuffer;
+
+
+
+var fs = require( 'fs' ) ;
+// var termkit = require( '../lib/termkit.js' ) ;
+
+var termkit = require('terminal-kit')
+var term;
+var ScreenBuffer = termkit.ScreenBuffer ;
+
+
+
+// Buffers
+var viewport , sprites = {} ;
+
+function init( callback )
+{
+  termkit.getDetectedTerminal( function( error , detectedTerm ) {
+
+    if ( error ) { throw new Error( 'Cannot detect terminal.' ) ; }
+
+    term = detectedTerm ;
+
+    viewport = ScreenBuffer.create( {
+      dst: term ,
+      width: Math.min( term.width ) ,
+      height: Math.min( term.height - 1 ) ,
+      y: 2
+    } ) ;
+
+    // createBackground() ;
+    // createSpaceship() ;
+
+    //term.fullscreen() ;
+    term.moveTo.eraseLine.bgWhite.green( 1 , 1 , 'Q/Ctrl-C: Quit\n' ) ;
+    term.hideCursor() ;
+    term.grabInput() ;
+    term.on( 'key' , inputs ) ;
+    callback(
+      term
+    ) ;
+  } ) ;
+}
+
+
+function inputs( key )
+{
+  switch ( key )
+  {
+    // case 'UP' :
+    //   sprites.spaceship.y -- ;
+    //   break ;
+    // case 'DOWN' :
+    //   sprites.spaceship.y ++ ;
+    //   break ;
+    // case 'LEFT' :
+    //   sprites.spaceship.x -- ;
+    //   break ;
+    // case 'RIGHT' :
+    //   sprites.spaceship.x ++ ;
+    //   break ;
+    case 'q':
+    case 'CTRL_C':
+      terminate() ;
+      break ;
+
+  }
+}
+
+
+function terminate()
+{
+  //term.fullscreen( false ) ;
+  term.hideCursor( false ) ;
+  term.grabInput( false ) ;
+
+  setTimeout( function() {
+    term.moveTo( 1 , term.height , '\n\n' ) ;
+    process.exit() ;
+  } , 100 ) ;
+}
+
+
+
+init((term) => {
+  console.log( 'okay hi'  );
+  term('term safs di')
+});
